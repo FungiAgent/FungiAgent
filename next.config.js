5e2e5a939d019080e3e07d1ef2904f362342f374
dreski <dreskirim@gmail.com>
2024-04-12 16:45:23 +0200

    environment variable maangement

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-   
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    ARBITRUM_API_KEY: process.env.ARBITRUM_API_KEY,
+    OPTIMISM_API_KEY: process.env.OPTIMISM_API_KEY,
+    POLYGON_API_KEY: process.env.POLYGON_API_KEY,
+    TAVILY_API_KEY: process.env.TAVILY_API_KEY,
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
